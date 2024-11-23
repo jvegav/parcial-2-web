360842314600aedf7abd112f32f88f1593a6ed1a
Josue Vega <j.vegav@uniandes.edu.co>
2024-11-23 12:54:16 -0500

    last controller

--- src/pacientemedico/pacientemedico.controller.ts
+++ src/pacientemedico/pacientemedico.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Param, Post, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+import { PacientemedicoService } from './pacientemedico.service';
 
-@Controller('pacientemedico')
-export class PacientemedicoController {}
+
+@Controller('pacientes')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class PacientemedicoController {
+    constructor(private readonly pacienteMedicoService: PacientemedicoService){}
+
+    @Post(':pacienteId/medicos/:medicoId')
+    async addArtworkMuseum(@Param('pacienteId') pacienteId: string, @Param('medicoId') medicoId: string){
+        return await this.pacienteMedicoService.addMedicoToPaciente(medicoId, pacienteId);
+    }
+
+
+}
