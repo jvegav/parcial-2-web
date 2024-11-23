1cca3e3d230da55d055e61ec3dedbfc1e7e1d6ff
Josue Vega <j.vegav@uniandes.edu.co>
2024-11-23 11:50:02 -0500

    Pacientes tests completos

--- src/pacientes/pacientes.service.spec.ts
+++ src/pacientes/pacientes.service.spec.ts
@@ -9,7 +9,7 @@ import { BusinessError } from '../shared/errorrs/business-errors';
 
 describe('PacienteService', () => {
   let service: PacientesService;
-  let repository: Repository<PacienteEntity>;
+  let pacienteRepository: Repository<PacienteEntity>;
  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,7 +18,7 @@ describe('PacienteService', () => {
     }).compile();
  
     service = module.get<PacientesService>(PacientesService);
-    repository = module.get<Repository<PacienteEntity>>(getRepositoryToken(PacienteEntity));
+    pacienteRepository = module.get<Repository<PacienteEntity>>(getRepositoryToken(PacienteEntity));
   });
    
   it('should be defined', () => {
@@ -54,12 +54,90 @@ describe('PacienteService', () => {
       
     } 
 
-    const saveSpy = jest.spyOn(repository, 'save').mockResolvedValueOnce(paciente);
+    const saveSpy = jest.spyOn(pacienteRepository, 'save').mockResolvedValueOnce(paciente);
 
     const result = await service.create(paciente);
 
     expect(result).toEqual(paciente);
     expect(saveSpy).toHaveBeenCalledWith(paciente);
   });
+
+
+
+  it('findAll: debe retornar todos los pacientes con sus relaciones', async () => {
+    const mockPacientes = [
+        { id: '1', nombre: 'Paciente 1', diagnosticos: [], medicos: [] },
+        { id: '2', nombre: 'Paciente 2', diagnosticos: [], medicos: [] },
+    ] as PacienteEntity[];
+
+    jest.spyOn(pacienteRepository, 'find').mockResolvedValue(mockPacientes);
+
+    const result = await service.findAll();
+    expect(result).toEqual(mockPacientes);
+    expect(pacienteRepository.find).toHaveBeenCalledWith({ relations: ['diagnosticos', 'medicos'] });
+});
+
+
+it('findOne: debe retornar un paciente por su ID', async () => {
+    const mockPaciente = { id: '1', nombre: 'Paciente 1', diagnosticos: [], medicos: [] } as PacienteEntity;
+
+    jest.spyOn(pacienteRepository, 'findOne').mockResolvedValue(mockPaciente);
+
+    const result = await service.findOne('1');
+    expect(result).toEqual(mockPaciente);
+    expect(pacienteRepository.findOne).toHaveBeenCalledWith({ where: { id: '1' }, relations: ['diagnosticos', 'medicos'] });
+});
+
+it('findOne: debe lanzar una excepción si el paciente no existe', async () => {
+    jest.spyOn(pacienteRepository, 'findOne').mockResolvedValue(undefined);
+
+    await expect(service.findOne('1')).rejects.toMatchObject({
+      type: BusinessError.NOT_FOUND,
+      message: 'The pacient with the given id was not found'
+      
+    });
+});
+
+
+it('delete: debe eliminar un paciente sin diagnosticos', async () => {
+    const mockPaciente = { id: '1', nombre: 'Paciente 1', diagnosticos: [], medicos: [] } as PacienteEntity;
+
+    jest.spyOn(pacienteRepository, 'findOne').mockResolvedValue(mockPaciente);
+    jest.spyOn(pacienteRepository, 'remove').mockResolvedValue(undefined);
+
+    await service.delete('1');
+    expect(pacienteRepository.remove).toHaveBeenCalledWith(mockPaciente);
+});
+
+it('delete: debe lanzar una excepción si el paciente no existe', async () => {
+    jest.spyOn(pacienteRepository, 'findOne').mockResolvedValue(undefined);
+
+    await expect(service.delete('1')).rejects.toMatchObject({
+      type: BusinessError.NOT_FOUND,
+      message: 'The pacient with the given id was not found'
+      
+    });
+
+    
+});
+
+it('delete: debe lanzar una excepción si el paciente tiene diagnosticos', async () => {
+    const mockPaciente = {
+        id: '1',
+        nombre: 'Paciente 1',
+        diagnosticos: [{ id: 'diag1', descripcion: 'Diagnóstico' }],
+        medicos: [],
+    } as PacienteEntity;
+
+    jest.spyOn(pacienteRepository, 'findOne').mockResolvedValue(mockPaciente);
+
+    
+
+    await expect(service.delete('1')).rejects.toMatchObject({
+      type: BusinessError.PRECONDITION_FAILED,
+      message: 'The pacient have one or more diagnosticos'
+      
+    });
+});
 });
 
